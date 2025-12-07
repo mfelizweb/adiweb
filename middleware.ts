f7ab019339f4f9881ee64b9b912d220cd549935f
mfeliz99 <mf.develop97@gmail.com>
2025-12-07 11:17:55 -0500

    Remove conflicting locale layout for Next 16 + next-intl

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,15 @@
-// middleware.ts
 import createMiddleware from "next-intl/middleware";
 import { locales, defaultLocale } from "./src/i18n/config";
 
 export default createMiddleware({
   locales,
   defaultLocale,
+  localePrefix: "always", // fuerza /en /es explícitamente
 });
 
 export const config = {
-  matcher: ["/((?!_next|favicon.ico|.*\\..*).*)"],
+  matcher: [
+    "/",        // solo la homepage
+    "/(en|es)/:path*",  // solo rutas con prefijo de idioma
+  ],
 };
